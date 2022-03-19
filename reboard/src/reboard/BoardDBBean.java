f217828b4e49948de8c6d6e5e550efa4edb86d05
rndjhds <86414343+rndjhds@users.noreply.github.com>
2022-03-19 22:12:50 +0900

    Add files via upload

--- reboard/src/reboard/BoardDBBean.java
+++ reboard/src/reboard/BoardDBBean.java
@@ -5,6 +5,8 @@ package reboard;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -94,14 +96,159 @@ public class BoardDBBean {
 		}
 				
 		return result;
+		
 	}
 	
 	
+	// 데이터 목록 : 데이터 10개 추출
+	public List<BoardDataBean> getList(int start, int end) {
+		List<BoardDataBean> list = new ArrayList<BoardDataBean>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = getConnection();
+			
+			String sql = "select * from (select rownum rnum, board.* from ";
+				   sql += " (select * from board order by ref desc, re_step asc) board )";
+				   sql += " where rnum >=? and rnum <=?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+			rs = pstmt.executeQuery();	// SQL문 실행
+			
+			while(rs.next()) {
+				BoardDataBean board = new BoardDataBean();
+				board.setNum(rs.getInt("num"));
+				board.setWriter(rs.getString("writer"));
+				board.setEmail(rs.getString("email"));
+				board.setSubject(rs.getString("subject"));
+				board.setPasswd(rs.getString("passwd"));
+				board.setReg_date(rs.getTimestamp("reg_date"));
+				board.setReadcount(rs.getInt("readcount"));
+				board.setRef(rs.getInt("ref"));
+				board.setRe_level(rs.getInt("re_level"));
+				board.setRe_step(rs.getInt("re_step"));
+				board.setContent(rs.getString("content"));
+				board.setIp(rs.getString("ip"));
+				
+				list.add(board);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!=null)try {rs.close();}catch (Exception e) {}
+			if(pstmt!=null)try {pstmt.close();}catch (Exception e) {}
+			if(con!=null)try {con.close();}catch (Exception e) {}
+		}
+		return list;
+	}
 	
 	
+	// 상세 페이지 : 조회수를 1증가 시키고 상세정보 구하기
+	public BoardDataBean updateContent(int num) {
+		BoardDataBean board = new BoardDataBean();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = getConnection();
+			
+			String sql ="update board set readcount=readcount+1 where num=?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.executeUpdate();	// SQL문 실행
+			
+			sql = "select * from board where num=?";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();	// SQL문 실행
+			
+			if(rs.next()) {
+				board.setNum(rs.getInt("num"));
+				board.setWriter(rs.getString("writer"));
+				board.setEmail(rs.getString("email"));
+				board.setSubject(rs.getString("subject"));
+				board.setPasswd(rs.getString("passwd"));
+				board.setReg_date(rs.getTimestamp("reg_date"));
+				board.setReadcount(rs.getInt("readcount"));
+				board.setRef(rs.getInt("ref"));
+				board.setRe_level(rs.getInt("re_level"));
+				board.setRe_step(rs.getInt("re_step"));
+				board.setContent(rs.getString("content"));
+				board.setIp(rs.getString("ip"));
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!=null)try {rs.close();}catch (Exception e) {}
+			if(pstmt!=null)try {pstmt.close();}catch (Exception e) {}
+			if(con!=null)try {con.close();}catch (Exception e) {}
+		}
+		
+		return board;
+	}
 	
 	
-	
+	// 댓글 작성
+	public int reply(BoardDataBean board) {
+		int result = 0;
+		
+		// 부모글에 대한 정보
+		int ref = board.getRef();
+		int re_step = board.getRe_step();
+		int re_level = board.getRe_level();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = getConnection();
+			
+			// 원문이 부모인 경우 : 원문의 re_step=0이기 때문에, 모든 댓글들의 re_step값이 1증가
+			// 댓글이 부모인 경우 : 부모의 re_step보다 큰 댓글만, re_step값이 1증가
+			
+			String sql = "update board set re_step=re_step+1 ";
+				   sql += " where ref=? and re_step > ?";
+				   
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1,ref);	// 부모의 ref
+			pstmt.setInt(2, re_step);	// 부모의 re_step
+			pstmt.executeUpdate();	// SQL문 실행
+			
+			sql = "insert into board values(board_seq.nextval,?,?,?,?,";
+			sql += "sysdate,?,?,?,?,?,?)";
+			
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, board.getWriter());
+			pstmt.setString(2, board.getEmail());
+			pstmt.setString(3, board.getSubject());
+			pstmt.setString(4, board.getPasswd());
+			pstmt.setInt(5, 0);		// readcount
+			pstmt.setInt(6, ref);	// ref
+			pstmt.setInt(7, re_step+1);		// re_step
+			pstmt.setInt(8, re_level+1);		// re_level
+			pstmt.setString(9, board.getContent());
+			pstmt.setString(10, board.getIp());
+			
+			result = pstmt.executeUpdate();	// SQL문 실행
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt!=null)try {pstmt.close();}catch (Exception e) {}
+			if(con!=null)try {con.close();}catch (Exception e) {}
+		}
+		
+		return result;
+	}
 	
 	
 	
